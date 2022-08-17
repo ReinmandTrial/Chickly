8265d7bf5e9f96c470c0334550ec8704fdf83377
NikCosmos <nikcosmos@gmail.com>
2022-08-17 21:56:00 +0300

    calc DONE

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -146,3 +146,95 @@ if (product) {
 }
 
 //========================================================================================================================================================
+
+//calc START========================================================================================================================================================
+const MAIN_CALC_EL = document.getElementById('interestScreme');
+//-----
+const calcBnbInputEL = document.getElementById('calcInputBnb');
+const calcBnbBtnPlusEL = document.getElementById('calcBnbPlus');
+const calcBnbBtnMinusEL = document.getElementById('calcBnbMinus');
+const calcBnbResultEl = document.getElementById('calcBnbResult');
+//-----
+const calcDaysInputEL = document.getElementById('calcInputDays');
+const calcDaysBtnPlusEL = document.getElementById('calcDaysPlus');
+const calcDaysBtnMinusEL = document.getElementById('calcDaysMinus');
+const calcDaysResultEl = document.getElementById('calcDaysResult');
+//-----
+const calcTotalPercentEl = document.getElementById('calcTotalPercent');
+const calcInvestmentSumInputEl = document.getElementById('calcInvestmentSum');
+const calcProfitSumEl = document.getElementById('profitSum');
+//-----
+const calcAllInput = MAIN_CALC_EL.querySelectorAll('input');
+
+calcAllInput.forEach((el) =>
+   el.addEventListener('input', function (e) {
+      this.value = this.value.replace(/[^\d.]/g, '');
+   })
+);
+
+calcInit();
+
+function calcInit() {
+   calcBnbInputEL.value = '400';
+   calcDaysInputEL.value = '1';
+   calcTotalPercent();
+}
+//========================================================================================================================================================
+calcBnbInputEL.addEventListener('input', calcBnbPercent);
+calcBnbBtnPlusEL.addEventListener('click', addBnbInput);
+calcBnbBtnMinusEL.addEventListener('click', subBnbInput);
+
+function addBnbInput() {
+   calcBnbInputEL.value = +calcBnbInputEL.value + 400;
+   calcBnbPercent();
+}
+function subBnbInput() {
+   if (calcBnbInputEL.value > 400) {
+      calcBnbInputEL.value = +calcBnbInputEL.value - 400;
+      calcBnbPercent();
+   }
+}
+function calcBnbPercent() {
+   const resultBnb = Math.trunc(+calcBnbInputEL.value / 400);
+   calcBnbResultEl.textContent = resultBnb / 10;
+   calcTotalPercent();
+}
+
+//========================================================================================================================================================
+calcDaysInputEL.addEventListener('input', calcDaysPercent);
+calcDaysBtnPlusEL.addEventListener('click', addDaysInput);
+calcDaysBtnMinusEL.addEventListener('click', subDaysInput);
+
+function addDaysInput() {
+   calcDaysInputEL.value = +calcDaysInputEL.value + 1;
+   calcDaysPercent();
+}
+function subDaysInput() {
+   if (calcDaysInputEL.value > 1) {
+      calcDaysInputEL.value = +calcDaysInputEL.value - 1;
+      calcDaysPercent();
+   }
+}
+function calcDaysPercent() {
+   const resultDays = calcDaysInputEL.value / 10;
+   calcDaysResultEl.textContent = resultDays;
+   calcTotalPercent();
+}
+
+//========================================================================================================================================================
+
+function calcTotalPercent() {
+   const resultPercent = +calcBnbResultEl.textContent + +calcDaysResultEl.textContent + 1;
+   calcTotalPercentEl.textContent = resultPercent.toFixed(1);
+   calcDailyProfit();
+}
+
+//========================================================================================================================================================
+calcInvestmentSumInputEl.addEventListener('input', calcDailyProfit);
+
+function calcDailyProfit() {
+   const profit = (calcInvestmentSumInputEl.value / 100) * +calcTotalPercentEl.textContent;
+   calcProfitSumEl.textContent = profit.toFixed(1);
+}
+
+//calc END========================================================================================================================================================
